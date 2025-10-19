1e65afa40c19a9a871de1ea13c1b671a172ac2a3
TerenceBeauregard <beauregardterence@gmail.com>
2025-10-19 02:24:03 +0200

    route par defaut visite

--- src/main/java/ytg/projetjavaytg/Controllers/api/VisiteController.java
+++ src/main/java/ytg/projetjavaytg/Controllers/api/VisiteController.java
@@ -1,8 +1,12 @@
 package ytg.projetjavaytg.Controllers.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ytg.projetjavaytg.Models.Visite;
 import ytg.projetjavaytg.Services.VisiteService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/visites")
@@ -12,5 +16,24 @@ public class VisiteController {
 
     public VisiteController(VisiteService visiteService) {
         this.visiteService = visiteService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Visite>> getAllVisites() {
+        List<Visite> visites = visiteService.getAllVisites();
+        return ResponseEntity.ok(visites);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Visite> getVisiteById(@PathVariable Long id) {
+        return visiteService.getVisiteById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Visite> createVisite(@RequestBody Visite visite) {
+        Visite createdVisite = visiteService.createVisite(visite);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdVisite);
     }
 }
