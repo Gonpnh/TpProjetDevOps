1dcbde880759964de7d5592d3894d969b4477114
TerenceBeauregard <beauregardterence@gmail.com>
2025-10-19 02:19:09 +0200

    route par defaut utilisateur

--- src/main/java/ytg/projetjavaytg/Controllers/api/UtilisateurController.java
+++ src/main/java/ytg/projetjavaytg/Controllers/api/UtilisateurController.java
@@ -1,16 +1,39 @@
 package ytg.projetjavaytg.Controllers.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ytg.projetjavaytg.Repositories.UtilisateurRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ytg.projetjavaytg.Models.Utilisateur;
+import ytg.projetjavaytg.Services.UtilisateurService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/utilisateurs")
 public class UtilisateurController {
 
-    private final UtilisateurRepository utilisateurRepository;
+    private final UtilisateurService utilisateurService;
 
-    public UtilisateurController(UtilisateurRepository utilisateurRepository) {
-        this.utilisateurRepository = utilisateurRepository;
+    public UtilisateurController(UtilisateurService utilisateurService) {
+        this.utilisateurService = utilisateurService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Utilisateur>> getAllUtilisateurs() {
+        List<Utilisateur> utilisateurs = utilisateurService.getAllUtilisateurs();
+        return ResponseEntity.ok(utilisateurs);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Utilisateur> getUtilisateurById(@PathVariable Long id) {
+        return utilisateurService.getUtilisateurById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Utilisateur> createUtilisateur(@RequestBody Utilisateur utilisateur) {
+        Utilisateur createdUtilisateur = utilisateurService.createUtilisateur(utilisateur);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUtilisateur);
     }
 }
