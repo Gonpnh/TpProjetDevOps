53ce7c5cf1b5342a33889988cb71f9995a05a538
TerenceBeauregard <beauregardterence@gmail.com>
2025-10-19 02:05:39 +0200

    route par defaut maitreapprentissage

--- src/main/java/ytg/projetjavaytg/Controllers/api/MaitreApprentissageController.java
+++ src/main/java/ytg/projetjavaytg/Controllers/api/MaitreApprentissageController.java
@@ -1,9 +1,12 @@
 package ytg.projetjavaytg.Controllers.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ytg.projetjavaytg.Repositories.MaitreApprentissageRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ytg.projetjavaytg.Models.MaitreApprentissage;
 import ytg.projetjavaytg.Services.MaitreApprentissageService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/maitreapprentissages")
@@ -13,5 +16,24 @@ public class MaitreApprentissageController {
 
     public MaitreApprentissageController(MaitreApprentissageService maitreApprentissageService) {
         this.maitreApprentissageService = maitreApprentissageService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MaitreApprentissage>> getAllMaitresApprentissage() {
+        List<MaitreApprentissage> maitres = maitreApprentissageService.getAllMaitresApprentissage();
+        return ResponseEntity.ok(maitres);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MaitreApprentissage> getMaitreApprentissageById(@PathVariable Long id) {
+        return maitreApprentissageService.getMaitreApprentissageById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<MaitreApprentissage> createMaitreApprentissage(@RequestBody MaitreApprentissage maitreApprentissage) {
+        MaitreApprentissage createdMaitre = maitreApprentissageService.createMaitreApprentissage(maitreApprentissage);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdMaitre);
     }
 }
--- src/main/java/ytg/projetjavaytg/Services/MaitreApprentissageService.java
+++ src/main/java/ytg/projetjavaytg/Services/MaitreApprentissageService.java
@@ -1,7 +1,12 @@
 package ytg.projetjavaytg.Services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ytg.projetjavaytg.Models.MaitreApprentissage;
 import ytg.projetjavaytg.Repositories.MaitreApprentissageRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MaitreApprentissageService {
@@ -10,5 +15,18 @@ public class MaitreApprentissageService {
 
     public MaitreApprentissageService(MaitreApprentissageRepository maitreApprentissageRepository) {
         this.maitreApprentissageRepository = maitreApprentissageRepository;
+    }
+
+    public List<MaitreApprentissage> getAllMaitresApprentissage() {
+        return maitreApprentissageRepository.findAll();
+    }
+
+    public Optional<MaitreApprentissage> getMaitreApprentissageById(Long id) {
+        return maitreApprentissageRepository.findById(id);
+    }
+
+    @Transactional
+    public MaitreApprentissage createMaitreApprentissage(MaitreApprentissage maitreApprentissage) {
+        return maitreApprentissageRepository.save(maitreApprentissage);
     }
 }
