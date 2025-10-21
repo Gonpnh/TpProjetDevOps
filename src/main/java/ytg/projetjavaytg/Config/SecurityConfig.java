adac37938f0dc2c3bb8ee1ea3c47d3bd87d112d1
YassineLC <lahmarcherif.yassine@gmail.com>
2025-10-21 12:39:04 +0200

    Modifying security config to allow register page without auth token

--- src/main/java/ytg/projetjavaytg/Config/SecurityConfig.java
+++ src/main/java/ytg/projetjavaytg/Config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/register", "/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
