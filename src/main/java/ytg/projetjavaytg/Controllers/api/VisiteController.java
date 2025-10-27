fb241ea87d18540c53c28d549ed971705c11df61
TerenceBeauregard <beauregardterence@gmail.com>
2025-10-27 21:17:43 +0100

    virgule mdt

--- src/main/java/ytg/projetjavaytg/Controllers/api/VisiteController.java
+++ src/main/java/ytg/projetjavaytg/Controllers/api/VisiteController.java
@@ -74,7 +74,7 @@ public class VisiteController {
             } else if (f.equals("distance") || f.equals("à distance") || f.equals("a distance") || f.equals("adistance")) {
                 savedFormat = "distance";
             } else {
-                throw new BadRequestException("Le format doit être : présentiel, hybride, distance")
+                throw new BadRequestException("Le format doit être : présentiel, hybride, distance");
             }
         }
 
